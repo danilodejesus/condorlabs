6e4040c964e15361febb0870ccaaea4ebd74286e
Danilo Viacava <daniloviacava.acidlab@latam.com>
2024-08-11 07:27:26 +0200

    translate to dansk and responsive

--- src/App.js
+++ src/App.js
@@ -9,6 +9,102 @@ import './App.css';
 import Slider from "react-slick";
 
 function App() {
+  const handleMouseEnter = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = "url('https://img.freepik.com/foto-gratis/experiencia-programacion-persona-que-trabaja-codigos-computadora_23-2150010125.jpg') center center no-repeat";
+
+    const consultoria = document.querySelector('.consultoria')
+    consultoria.style.opacity = '0'
+    const sikkerhed = document.querySelector('.sikkerhed')
+    sikkerhed.style.opacity = '0'
+    const billeder = document.querySelector('.billeder')
+    billeder.style.opacity = '0'
+  }
+
+  const handleMouseLeave = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = 'black';
+
+    const consultoria = document.querySelector('.consultoria')
+    consultoria.style.opacity = '1'
+    const sikkerhed = document.querySelector('.sikkerhed')
+    sikkerhed.style.opacity = '1'
+    const billeder = document.querySelector('.billeder')
+    billeder.style.opacity = '1'
+  }
+
+  const mouseEnterConsultoria = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = "url('https://assets-global.website-files.com/60c3d2d198ecc411de7f67bf/61020ab9005241c0e17a36a2_consultoria-em-tecnologia-da-informacao-o-que-e-e-quando-contratar.png') center center no-repeat";
+
+    const consultoria = document.querySelector('.web')
+    consultoria.style.opacity = '0'
+    const sikkerhed = document.querySelector('.sikkerhed')
+    sikkerhed.style.opacity = '0'
+    const billeder = document.querySelector('.billeder')
+    billeder.style.opacity = '0'
+  }
+
+  const mouseLeaveConsultoria = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = 'black';
+
+    const consultoria = document.querySelector('.web')
+    consultoria.style.opacity = '1'
+    const sikkerhed = document.querySelector('.sikkerhed')
+    sikkerhed.style.opacity = '1'
+    const billeder = document.querySelector('.billeder')
+    billeder.style.opacity = '1'
+  }
+
+  const mouseEnterSikkerhed = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = "url('https://static.vecteezy.com/system/resources/thumbnails/008/279/107/small/padlock-with-keyhole-in-data-security-on-circuit-modern-safety-digital-concept-photo.jpg') center center no-repeat";
+
+    const consultoria = document.querySelector('.web')
+    consultoria.style.opacity = '0'
+    const sikkerhed = document.querySelector('.consultoria')
+    sikkerhed.style.opacity = '0'
+    const billeder = document.querySelector('.billeder')
+    billeder.style.opacity = '0'
+  }
+
+  const mouseLeaveSikkerhed = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = 'black';
+
+    const consultoria = document.querySelector('.web')
+    consultoria.style.opacity = '1'
+    const sikkerhed = document.querySelector('.consultoria')
+    sikkerhed.style.opacity = '1'
+    const billeder = document.querySelector('.billeder')
+    billeder.style.opacity = '1'
+  }
+
+  const mouseEnterBilleder = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = "url('https://149355317.v2.pressablecdn.com/wp-content/uploads/2019/09/drones-construction-1024x768.jpg') center center no-repeat";
+
+    const consultoria = document.querySelector('.web')
+    consultoria.style.opacity = '0'
+    const sikkerhed = document.querySelector('.consultoria')
+    sikkerhed.style.opacity = '0'
+    const billeder = document.querySelector('.sikkerhed')
+    billeder.style.opacity = '0'
+  }
+
+  const mouseLeaveBilleder = () => {
+    const serviceWeb = document.querySelector('.Services')
+    serviceWeb.style.background = 'black';
+
+    const consultoria = document.querySelector('.web')
+    consultoria.style.opacity = '1'
+    const sikkerhed = document.querySelector('.consultoria')
+    sikkerhed.style.opacity = '1'
+    const billeder = document.querySelector('.sikkerhed')
+    billeder.style.opacity = '1'
+  }
+
   var settings = {
     dots: false,
     arrows: true,
@@ -55,39 +151,40 @@ function App() {
             <li>
               <a href="/diseno-web/">
                 <figure class="df">
-                  <h2>DISEÑO WEB</h2>
+                  <h2>WEBDESIGN</h2>
                 </figure>
-                <p>Nuestro servicio clave son páginas web profesionales y únicas. <br/>
-                  <span>Disponible en dispositivos móviles, listo para SEO (Google).</span>
+                <p>Vores nøgleservice er professionelle og unikke hjemmesider. <br/>
+                  <span>Tilgængelig på mobile enheder, SEO (Google) klar.</span>
                 </p>
               </a>
             </li>
             <li>
               <a href="/portafolio/">
                 <figure class="df">
-                  <h2>CONSULTORÍA TECNOLÓGICA</h2>
+                  <h2>TEKNOLOGISK RÅDGIVNING</h2>
                 </figure>
                 <p>
-                  Cómo la empresa ayuda a optimizar procesos utilizando tecnología avanzada.
+                  Hvordan virksomheden hjælper med at optimere processer ved hjælp af avanceret teknologi.
                 </p>
               </a>
             </li>
             <li>
               <a href="/viaje-de-trabajo/">
                 <figure class="df">
-                  <h2>HOSTING Y SEGURIDAD</h2>
+                  <h2>HOSTING OG SIKKERHED</h2>
                 </figure>
                 <p>
-                  Ofrecemos servicios avanzados de alojamiento web y ciberseguridad.
+                  Vi tilbyder avancerede webhosting- og cybersikkerhedstjenester.
                 </p>
               </a>
             </li>
             <li>
               <a href="/imagenes-de-drones/">
                 <figure class="df">
-                  <h2>IMÁGENES DE DRONES</h2>
+                  <h2>DRONE BILLEDER</h2>
                 </figure>
-                <p>Brindamos servicios de fotografía y video aéreo con drones.  <span>Certificados con licencia para conducir drones.</span>
+                <p>Vi leverer luftfotografering og videotjenester med droner.
+                  <span>Drone kørekort certifikater.</span>
                 </p>
               </a>
             </li>
@@ -97,57 +194,69 @@ function App() {
 
       <section className='Introduction'>
         <p className=''>
-          Condor Labs busca conectar la majestuosidad del cóndor con la tecnología avanzada, <br/> destacando un compromiso con la excelencia, la innovación y la sostenibilidad, <br/> todos valores importantes tanto en el ámbito tecnológico  <br/> como en la cultura danesa.
+        Condor Labs søger at forbinde kondorens majestæt med avanceret teknologi, <br/> 
+        der fremhæver en forpligtelse til ekspertise, innovation og bæredygtighed, <br/> 
+        alle vigtige værdier​​både på det teknologiske  <br/> 
+        område og i Dansk Kultur.
         </p>
       </section>
 
       <section className='Services'>
-        <div className='container flex f-start'>
-          <figure className='Service'>
+        <div className='container flex f-start relative'>
+          <figure 
+            className='Service web' 
+            onMouseOver={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
             <img src={responsive} alt="" width="60" />
             <figcaption>
-              Desarrollo de Sistemas
+              Systemudvikling
               <p>
-                E-commerce <br/>
-                Desarrollo web <br/>
-                Aplicaciones móviles <br/>
-                Actualización de WordPress<br/>
+                E-handel <br/>
+                Webudvikling <br/>
+                Mobilapplikationer <br/>
+                WordPress-opdatering<br/>
               </p>
             </figcaption>
           </figure>
-          <figure className='Service'>
+          <figure className='Service consultoria'
+            onMouseOver={mouseEnterConsultoria}
+            onMouseLeave={mouseLeaveConsultoria}>
             <img src={consultoria} alt="" width="60" />
             <figcaption>
-              Consultoría Tecnológica
+              Teknologirådgivning
               <p>
-                Transformación Digital <br/>
-                Integración de Sistemas <br/>
-                Asesoramiento en Software <br/>
-                Actualización de WordPress<br/>
+                Systemintegration <br/>
+                Softwarerådgivning <br/>
+                Digital transformation <br/>
+                WordPress-opdatering<br/>
               </p>
             </figcaption>
           </figure>
-          <figure className='Service'>
+          <figure className='Service sikkerhed'
+            onMouseOver={mouseEnterSikkerhed}
+            onMouseLeave={mouseLeaveSikkerhed}>
             <img src={hosting} alt="" width="60" />
             <figcaption>
-              Hosting y Seguridad
+              Hosting og sikkerhed
               <p>
-                Alojamiento web <br/>
-                Pruebas de Penetración <br/>
-                Monitoreo de Seguridad 24/7 <br/>
-                Seguridad de Aplicaciones Web <br/>
+                Webhosting <br/>
+                Penetrationstests <br/>
+                24/7 Sikkerhedsovervågning <br/>
+                Webapplikationssikkerhed <br/>
               </p>
             </figcaption>
           </figure>
-          <figure className='Service'>
+          <figure className='Service billeder'
+            onMouseOver={mouseEnterBilleder}
+            onMouseLeave={mouseLeaveBilleder}>
             <img src={drone} alt="" width="60" />
             <figcaption>
-              Imágenes de drones
+              Drone billeder
               <p>
-                Fotografía y video <br/>
-                Seguridad y vigilancia <br/>
-                Precisión en agricultura <br/>
-                Inspección de construcción <br/>
+                Byggeeftersyn <br/>
+                Fotografi og video <br/>
+                Præcision i landbruget <br/>
+                Sikkerhed og overvågning <br/>
               </p>
             </figcaption>
           </figure>
@@ -157,47 +266,47 @@ function App() {
       <section className='Stories'>
         <div className='container flex evenly'>
           <h4>
-            Opiniones <br/>
-            <img src="https://daniloviacava-40daf.web.app/static/img/Fb-rates.png"/> <br/>
-            <i>Clientes satisfechos</i>
+            Meninger <br/>
+            <img src="https://daniloviacava-40daf.web.app/static/img/Fb-rates.png" alt=""/> <br/>
+            <i>Tilfredse kunder</i>
           </h4>
 
           <div className='Stories-slider'>
             <Slider {...settings}>
               <div className='Stories-slide'>
-                <h3>Mario Gabaldoni</h3>
+                <h3>Anna Bøttern</h3>
                 <p>
-                  Me desarrollaron un sistema intuitivo y facil de utilizar, pude reducir los tiempos en mis procesos de venta.
+                  De udviklede et intuitivt og brugervenligt system til mig, jeg var i stand til at reducere tiden i mine salgsprocesser.
                 </p>
               </div>
               <div className='Stories-slide'>
-                <h3>Andrew Tate</h3>
+                <h3>Henrik Larsen</h3>
                 <p>
-                  Demostró calidad y ser ágil con los cambios que se necesitaban con rapidez.
+                  Opmærksomheden på detaljerne, den dybe forståelse af vores behov og evnen til at levere skalerbare løsninger til tiden var enestående. Vi kunne ikke være mere tilfredse med resultatet.
                 </p>
               </div>
               <div className='Stories-slide'>
-                <h3>Inka Pachacutec</h3>
+                <h3>Charlotte Hansen</h3>
                 <p>
-                  Danilo es millonario en un mes, consiguió un trabajo con drones y haciendo sistemas.
+                  De hjalp os med at designe og udvikle et helt skræddersyet flådestyringssystem, der giver os mulighed for at optimere vores logistik.
                 </p>
               </div>
               <div className='Stories-slide'>
-                <h3>Jorge Luna</h3>
+                <h3>Cristina Montoro</h3>
                 <p>
-                  Eres un chucha en tu chamba, sigue dando calidad y buenas satisfacciones 5 estrellas a tus clientes.
+                  Vi var imponerede over den kreativitet og innovative tilgang, som Condor Labs bragte til udviklingen af ​​vores mobilapp. Ikke alene levede de op til vores forventninger, de overgik dem ved at introducere funktioner, som vi ikke havde overvejet, men som har været nøglen til succesen for vores app på markedet.
                 </p>
               </div>
               <div className='Stories-slide'>
-                <h3>Nicolas Paucar</h3>
+                <h3>Isak Ringheim</h3>
                 <p>
-                  Acepta todo de ti y proyecta todo lo que quieres lograr desde el corazón.
+                  E-handelsplatformen, som Condor Labs udviklede til os, har transformeret den måde, vi driver forretning på online. Deres team forstod perfekt vores krav og gjorde dem til en intuitiv, hurtig og sikker løsning.
                 </p>
               </div>
               <div className='Stories-slide'>
-                <h3>Roberto Neo Tolteca</h3>
+                <h3>Christian Sørensen</h3>
                 <p>
-                  Rompe la matrix, cree en tí y juega pensando siempre desde el corazón, eres serio y comprometido.
+                  De hjalp os med at udvikle en mobil aftalebestillingsapp, vi har set en markant stigning i konverteringer og en forbedring af brugeroplevelsen. Jeg vil helt klart anbefale deres tjenester til enhver virksomhed, der ønsker at skille sig ud på det digitale marked.
                 </p>
               </div>
             </Slider>
@@ -207,7 +316,7 @@ function App() {
 
       <section className='Connect'>
         <div className='container relative'>
-          <h4>Contáctanos</h4>
+          <h4>Kontakt os</h4>
           <div className='flex'>
             <div className='Connect-form'>
               <form>
@@ -221,17 +330,22 @@ function App() {
               <ul>
                 <li>
                   <p>
-                    - Dirección: Tofthøjvej 18 2th - Denmark
+                    Adresse: Tofthøjvej 18 2th
                   </p>
                 </li>
                 <li>
                   <p>
-                    - Celular: +45 60 90 22 94
+                    Email: danilojesusv@gmail.com
                   </p>
                 </li>
                 <li>
                   <p>
-                    - CVR: 44989646
+                    Cellulær: +45 60 90 22 94
+                  </p>
+                </li>
+                <li>
+                  <p>
+                    CVR: 44989646
                   </p>
                 </li>
               </ul>
@@ -257,7 +371,7 @@ function App() {
           </div>
           <div className='footer-right'>
             <p>
-              Privacy Policy | © CondorLabs 2024
+              Privatlivspolitik | © CondorLabs 2024
             </p>
           </div>
         </div>
